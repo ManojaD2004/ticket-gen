b8551f4ff2670a963db372d7cfa429e0c2866fda
ManojaD2004 <manojadkc2004@gmail.com>
2024-06-08 12:12:51 +0530

    Third

--- client/app/owner/page.js
+++ client/app/owner/page.js
@@ -1,10 +1,36 @@
+"use client";
+import { useState } from "react";
 
 
 export default function Home() {
+    const [name, setName] = useState("");
+    const [ename, setEname] = useState("");
+
+    const genTicker = async  (e) => {
+        e.preventDefault();
+                        await fetch("http://localhost:5000/generate", {
+                            headers: {
+                                'Accept': 'application/json',
+                                'Content-Type': 'application/json'
+                              },
+                            body: JSON.stringify({
+                                name: name, 
+                                ename: ename
+                            }),
+                            method: "POST",
+                    }).then(response => response.json().then(data => window.alert("Event Created!")))
+                }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <h1>Owner  - <span>Create Your Event</span></h1>
+        <h1 className="text-3xl font-bold">Owner  - <span>Create Your Event</span></h1>
+        <form className="mt-3 flex flex-col space-y-3">
+            <label className="text-2xl mr-3">Enter Your Event Name: </label>
+            <input onChange={(e) => {setEname(e.target.value)}} value={ename} type="text" className="bg-slate-100 border-0 outline-0" />
+            <label className="text-2xl mr-3">Enter Your Name: </label>
+            <input onChange={(e) => {setName(e.target.value)}} value={name} type="text" className="bg-slate-100 border-0 outline-0" />
+            <button className="bg-green-400" onClick={genTicker}>Submit</button>
+        </form>
       </div>
     </main>
   );
--- server/index.js
+++ server/index.js
@@ -13,10 +13,36 @@ app.get("/home", (req,res) => {
 });
 
 app.post("/generate", (req,res) => {
-	const ticketGen = uuidv4();
-	const dir = "./tickets/" + req.body.dir;
+	console.log(req.body)
+	const dir = "./tickets/" + req.body.ename;
 	fs.mkdirSync(dir);
+	fs.writeFileSync(dir + "/json.json", '{"tickets": []}', {encoding:"utf8"});
+	res.send({ "dir": dir});
+});
+
+app.post("/generate/ticket", (req,res) => {
+	const ticketGen = uuidv4();
+	console.log(req.body)
+	const dir = "./tickets/" + req.body.ename + "/json.json";
+	const data = JSON.parse(fs.readFileSync(dir, {encoding:"utf8"}));
+	data.tickets.push(ticketGen);
+	fs.writeFileSync(dir, JSON.stringify(data), {encoding:"utf8"});
 	res.send({"ticket": ticketGen, "dir": dir});
+});
+
+app.post("/verify/ticket", (req,res) => {
+	console.log(req.body)
+	const dir = "./tickets/" + req.body.ename + "/json.json";
+	const data = JSON.parse(fs.readFileSync(dir, {encoding:"utf8"}));
+	const isTrue = data.tickets.indexOf(req.body.ticket) > -1;
+	fs.writeFileSync(dir, JSON.stringify(data), {encoding:"utf8"});
+	res.send({"isTrue": isTrue});
+});
+
+
+app.get("/getevents", (req,res) => {
+	const dir = "./tickets";
+	res.send({"dir": fs.readdirSync(dir)});
 });
 
 app.listen(PORT, () => {
